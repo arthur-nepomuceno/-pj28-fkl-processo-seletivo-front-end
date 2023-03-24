05e2fce024802c6745598df1900a202318fe7f86
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2023-03-24 16:46:46 -0300

    :sparkles:Feat: edição de cadastros

--- src/paginas/EditarCadastro/EditarCadastro.tsx
+++ src/paginas/EditarCadastro/EditarCadastro.tsx
@@ -1,9 +1,12 @@
 import "../Cadastro/cadastro.css";
 import API from "../../api/Api";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Cadastro } from "../../types/cadastroTypes";
 
-export function Cadastro() {
+export function EditarCadastro() {
+
+    const { id } = useParams();
 
     const [doc, setDoc] = useState('');
     const [contato, setContato] = useState('');
@@ -18,10 +21,33 @@ export function Cadastro() {
     const [cep, setCep] = useState('');
     const navigate = useNavigate();
 
-    async function enviarCadastro(event: any) {
+    async function buscarCadastroPorId(id: number | any) {
+
+        try {
+            const lista = await API.get('/lista-de-cadastros');
+            const filtragem = lista.data.filter((cad: Cadastro) => cad.id === +id);
+            const cadastro = filtragem[0];
+            setDoc(cadastro.doc)
+            setContato(cadastro.contato)
+            setNomeFantasia(cadastro.nomeFantasia)
+            setTelefoneContato(cadastro.telefoneContato)
+            setRazaoSocial(cadastro.razaoSocial)
+            setEmailContato(cadastro.emailContato)
+            setEndereco(cadastro.endereco)
+            setBairro(cadastro.bairro)
+            setCidade(cadastro.cidade)
+            setUf(cadastro.uf)
+            setCep(cadastro.cep)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function editarCadastro(event: any) {
         event.preventDefault();
 
         const body = {
+            id: Number(id),
             doc,
             contato,
             nomeFantasia,
@@ -36,7 +62,7 @@ export function Cadastro() {
         }
 
         try {
-            const response = await API.post('/cadastro', body);
+            const response = await API.post('/editar', body);
             navigate('/lista-de-cadastros')
             console.log(response.data);
         } catch (error) {
@@ -44,7 +70,8 @@ export function Cadastro() {
         }
     }
 
-    async function cancelarEnvio(){
+    async function cancelarEnvio() {
+        
         setDoc('');
         setContato('');
         setNomeFantasia('');
@@ -60,45 +87,48 @@ export function Cadastro() {
         navigate('/lista-de-cadastros')
     }
 
+
+    useEffect(() => { buscarCadastroPorId(id) }, [])
+
     return (
         <div className="container align-self-center p-3 bg-gray">
-            <h5 className="m-3">Cadastro de Pessoas</h5>
-            <form className="row g-3 m-3 p-3 bg-white" onSubmit={enviarCadastro}>
+            <h5 className="m-3">Editar Cadastro</h5>
+            <form className="row g-3 m-3 p-3 bg-white" onSubmit={editarCadastro}>
                 <div className="col-md-6">
                     <label htmlFor="inputEmail4" className="form-label">CPF ou CNPJ</label>
-                    <input type="number" className="form-control" id="inputEmail4" onChange={e => { setDoc(e.target.value) }} required />
+                    <input type="number" className="form-control" id="inputEmail4" value={doc} onChange={e => { setDoc(e.target.value) }} required />
                 </div>
                 <div className="col-md-6">
                     <label htmlFor="inputPassword4" className="form-label">Contato</label>
-                    <input type="text" className="form-control" id="inputPassword4" onChange={e => { setContato(e.target.value) }} required />
+                    <input type="text" className="form-control" id="inputPassword4" value={contato} onChange={e => { setContato(e.target.value) }} required />
                 </div>
                 <div className="col-md-6">
                     <label htmlFor="inputEmail4" className="form-label">Nome Fantasia</label>
-                    <input type="text" className="form-control" id="inputEmail4" onChange={e => { setNomeFantasia(e.target.value) }} required />
+                    <input type="text" className="form-control" id="inputEmail4" value={nomeFantasia} onChange={e => { setNomeFantasia(e.target.value) }} required />
                 </div>
                 <div className="col-md-6">
                     <label htmlFor="inputPassword4" className="form-label">Telefone do Contato</label>
-                    <input type="text" className="form-control" id="inputPassword4" onChange={e => { setTelefoneContato(e.target.value) }} required />
+                    <input type="text" className="form-control" id="inputPassword4" value={telefoneContato} onChange={e => { setTelefoneContato(e.target.value) }} required />
                 </div>
                 <div className="col-md-6">
                     <label htmlFor="inputEmail4" className="form-label">Razão Social</label>
-                    <input type="text" className="form-control" id="inputEmail4" onChange={e => { setRazaoSocial(e.target.value) }} required />
+                    <input type="text" className="form-control" id="inputEmail4" value={razaoSocial} onChange={e => { setRazaoSocial(e.target.value) }} required />
                 </div>
                 <div className="col-md-6">
                     <label htmlFor="inputPassword4" className="form-label">E-mail do Contato</label>
-                    <input type="email" className="form-control" id="inputPassword4" onChange={e => { setEmailContato(e.target.value) }} required />
+                    <input type="email" className="form-control" id="inputPassword4" value={emailContato} onChange={e => { setEmailContato(e.target.value) }} required />
                 </div>
                 <div className="col-6">
                     <label htmlFor="inputAddress" className="form-label">Endereço</label>
-                    <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" onChange={e => { setEndereco(e.target.value) }} required />
+                    <input type="text" className="form-control" id="inputAddress" value={endereco} placeholder="1234 Main St" onChange={e => { setEndereco(e.target.value) }} required />
                 </div>
                 <div className="col-6">
                     <label htmlFor="inputAddress2" className="form-label">Bairro</label>
-                    <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" onChange={e => { setBairro(e.target.value) }} required />
+                    <input type="text" className="form-control" id="inputAddress2" value={bairro} placeholder="Apartment, studio, or floor" onChange={e => { setBairro(e.target.value) }} required />
                 </div>
                 <div className="col-md-6">
                     <label htmlFor="inputCity" className="form-label">Cidade</label>
-                    <input type="text" className="form-control" id="inputCity" onChange={e => { setCidade(e.target.value) }} required />
+                    <input type="text" className="form-control" id="inputCity" value={cidade} onChange={e => { setCidade(e.target.value) }} required />
                 </div>
                 <div className="col-md-4">
                     <label htmlFor="inputState" className="form-label">UF</label>
@@ -109,14 +139,14 @@ export function Cadastro() {
                 </div>
                 <div className="col-md-2">
                     <label htmlFor="inputZip" className="form-label">CEP</label>
-                    <input type="text" className="form-control" id="inputZip" onChange={e => { setCep(e.target.value) }} required />
+                    <input type="text" className="form-control" id="inputZip" value={cep} onChange={e => { setCep(e.target.value) }} required />
                 </div>
                 <div className='d-flex justify-content-end'>
                     <div className="col-1">
                         <button type="button" className="btn btn-primary" onClick={cancelarEnvio}>Cancelar</button>
                     </div>
                     <div className="col-1">
-                        <button type="submit" className="btn btn-primary">Salvar</button>
+                        <button type="submit" className="btn btn-primary">Editar</button>
                     </div>
                 </div>
             </form>
--- src/rotas/Rotas.tsx
+++ src/rotas/Rotas.tsx
@@ -1,11 +1,14 @@
-import { RotasCadastro } from "./RotasCadastro"
-import { RotasLista } from "./RotasLista"
+import { Routes, Route } from "react-router-dom";
+import { Lista } from "../paginas/Lista/Lista";
+import { Cadastro } from "../paginas/Cadastro/Cadastro";
+import { EditarCadastro } from "../paginas/EditarCadastro/EditarCadastro";
 
 export function Rotas() {
     return (
-        <>
-            <RotasCadastro />
-            <RotasLista />
-        </>
+        <Routes>
+            <Route path='/cadastro' element={<Cadastro />} />
+            <Route path='/lista-de-cadastros' element={<Lista />} />
+            <Route path='/editar/:id' element={<EditarCadastro />} />
+        </Routes>
     )
 }
