021ce03cf8c09b095408bc81b55b8d29695ea4df
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2023-03-24 13:36:31 -0300

    :sparkles:Feat: visualização de cadastros e filtro de pesquisa

--- src/paginas/Lista/Lista.tsx
+++ src/paginas/Lista/Lista.tsx
@@ -1,25 +1,63 @@
+import API from "../../api/Api";
+import { useEffect, useState } from "react";
+import { Cadastro } from "../../types/cadastroTypes";
+
 export function Lista() {
+
+    const [cadastros, setCadastros] = useState([]);
+    const [listagem, setListagem] = useState([])
+    const [filtro, setFiltro] = useState('')
+
+    async function buscarCadastros() {
+
+        try {
+            const lista = await API.get('/lista-de-cadastros');
+            setCadastros(lista.data);
+            setListagem(lista.data);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => { buscarCadastros() }, [])
+
+    async function filtrarCadastros(event: any) {
+        event.preventDefault();
+
+        if (filtro === '') {
+            setListagem(cadastros);
+        } else {
+            const lista = cadastros.filter(
+                (cad: Cadastro | any) => cad.id === +filtro || cad.nomeFantasia === filtro || cad.razaoSocial === filtro
+            );
+
+            setListagem(lista);
+        }
+
+        return;
+    }
+
     return (
         <div className="container align-self-center p-3 bg-gray">
             <h5 className="m-3">Listagem de Pessoas</h5>
-            <form className="row g-3 m-3 p-3 bg-white">
+            <form className="row g-3 m-3 p-3 bg-white" onSubmit={filtrarCadastros}>
                 <div className="col-md-6">
                     <label htmlFor="inputEmail4" className="form-label">Código</label>
-                    <input type="email" className="form-control" id="inputEmail4"/>
+                    <input type="number" className="form-control" id="inputNumber4" onChange={e => { setFiltro(e.target.value) }} />
                 </div>
                 <div className="col-md-6">
                     <label htmlFor="inputPassword4" className="form-label">Nome Fantasia</label>
-                    <input type="password" className="form-control" id="inputPassword4"/>
+                    <input type="text" className="form-control" id="inputText4" onChange={e => { setFiltro(e.target.value) }} />
                 </div>
                 <div className="col-md-9">
                     <label htmlFor="inputEmail4" className="form-label">Razão Social</label>
-                    <input type="email" className="form-control" id="inputEmail4"/>
+                    <input type="text" className="form-control" id="inputText4" onChange={e => { setFiltro(e.target.value) }} />
                 </div>
-                <div className="col-md-2 border">
-                    <div className="col d-flex border">
+                <div className="col-md-2">
+                    <div className="col d-flex">
                         <button type="submit" className="btn btn-primary">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search me-1" viewBox="0 0 16 16">
-                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                             </svg>
                             Filtrar
                         </button>
@@ -35,46 +73,39 @@ export function Lista() {
                         </button>
                     </div>
                 </div>
-
-                <div className="col m-3 p-3 border-top border-bottom border-secondary">
-                    <div className="table-responsive">
-                        <table className="table">
-                            <thead className="table-secondary">
-                                <tr>
-                                    <th scope="col">Código</th>
-                                    <th scope="col">Nome</th>
-                                    <th scope="col">Cidade</th>
-                                    <th scope="col">UF</th>
-                                    <th scope="col">Ações</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>00001</td>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <td>SC</td>
-                                </tr>
-                                <tr>
-                                    <td>00002</td>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                    <td>RJ</td>
-                                </tr>
-                                <tr>
-                                    <td>00003</td>
-                                    <td>Larry</td>
-                                    <td>the Bird</td>
-                                    <td>@twitter</td>
-                                    <td>MG</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
             </form>
+
+            <div className="col m-3 p-3 border-top border-bottom border-secondary bg-white">
+                <div className="table-responsive">
+                    <table className="table">
+                        <thead className="table-secondary">
+                            <tr>
+                                <th scope="col">Código</th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Cidade</th>
+                                <th scope="col">UF</th>
+                                <th scope="col">Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                listagem
+                                    .map((item: Cadastro | any, index) =>
+                                        <>
+                                            <tr>
+                                                <td>{item.id}</td>
+                                                <td>{item.contato}</td>
+                                                <td>{item.cidade}</td>
+                                                <td>{item.uf}</td>
+                                                <td>editar/deletar</td>
+                                            </tr>
+                                        </>
+                                    )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-    )   
+    )
 }
---
+++ src/types/cadastroTypes.tsx
@@ -0,0 +1,19 @@
+
+interface Cadastro {
+    id: Number,
+    doc: string,
+    contato: string,
+    nomeFantasia: string,
+    telefoneContato: number,
+    razaoSocial: string,
+    emailContato: string,
+    endereco: string,
+    bairro: string,
+    cidade: string,
+    uf: string,
+    cep: string
+}
+
+export type {
+    Cadastro
+}
